e7270c43f250b7622cc8ae54c7a1907d7a798933
Akiyafaith <akiyafaith@gmail.com>
2023-01-26 14:45:48 -0800

    printf

--- functions.c
+++ functions.c
@@ -85,6 +85,28 @@ int flags, int width, int precision, int size)
  * @size: Size specifier
  * Return: Number of chars printed
  */
+int print_percent(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	return (write(1, "%%", 1));
+}
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_int(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
